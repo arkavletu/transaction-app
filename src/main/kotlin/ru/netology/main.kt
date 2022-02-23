8e1c02cc265d1601497b562e5cf4089d5f105e48
Alina Prokofeva <whitestring@inbox.ru>
2022-02-23 17:43:25 +0300

    checked

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -8,17 +8,17 @@ const val MASTERCARD = "Mastercard"
 const val VK = "VkPay"
 const val MIR = "Mir"
 const val MIN_COMMISSION = 3500 // for visa, mir
-const val PERCENT_V_M = 0.75 // % for visa, mir
+const val PERCENT_V_M = 0.75/100 // % for visa, mir
 const val LIMIT = 75000 * 100 // limit for maestro, mastercard for zero commission
-const val PERCENT_M_M = 0.6 // % for maestro, mastercard
+const val PERCENT_M_M = 0.6/100 // % for maestro, mastercard
 const val ADDITIONAL_PAY = 2000 // add to commission for maestro, mastercard
 
 fun main() {
-    val result = checkCommission(currentSum = 90)
+    val result = checkCommission(90)
     println(formattedMessage(result))
 }
 
-fun checkCommission(type: String = VK, previousSum: Int = 0, currentSum: Int): Int {
+fun checkCommission(currentSum: Int, type: String = VK, previousSum: Int = 0): Int {
     return when (type) {
         MAESTRO, MASTERCARD -> checkPreviousTransactions(previousSum, currentSum)
         VISA, MIR -> checkMinCommission(currentSum)
@@ -28,11 +28,11 @@ fun checkCommission(type: String = VK, previousSum: Int = 0, currentSum: Int): I
 }
 
 fun checkPreviousTransactions(previousSum: Int, currentSum: Int): Int {
-    return if (previousSum > LIMIT) (currentSum * (PERCENT_M_M / 100) + ADDITIONAL_PAY).roundToInt() else 0
+    return if (previousSum > LIMIT) (currentSum * (PERCENT_M_M) + ADDITIONAL_PAY).roundToInt() else 0
 }
 
 fun checkMinCommission(sum: Int): Int {
-    val amount = (sum * (PERCENT_V_M / 100)).roundToInt()
+    val amount = (sum * PERCENT_V_M).roundToInt()
     return if (amount > MIN_COMMISSION) amount else MIN_COMMISSION
 
 }
